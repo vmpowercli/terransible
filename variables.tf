e14459c017c3f407e7ac042df6d9117059b0db81
vmpowercli <vmpowercli@gmail.com>
2018-04-29 10:01:37 -0700

    Modified Variables File

--- variables.tf
+++ variables.tf
@@ -1,8 +1,4 @@
 variable "aws_region"{}
 variable "aws_profile"{}
-<<<<<<< HEAD
 data "aws_availability_zones" "available"{}
-=======
-#data "aws_availabilty_zones" "available"{}
->>>>>>> 50b52c7e22bb2322e2c37c87d6cea3504ec56876
 variable "vpc_cidr"{}
