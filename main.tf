d0c771bc2057ef473b21126a2679340d07b8e824
vmpowercli <vmpowercli@gmail.com>
2018-04-29 13:35:09 -0400

    Added Subnets

--- main.tf
+++ main.tf
@@ -82,7 +82,28 @@ resource "aws_internet_gateway" "wp_internet_gateway"{
 	Name = "wp_igw"	
 }
 }
-output "vpc_id" {
-  value = "${aws_vpc.wp_vpc.id}"
+
+## Route Tables ###
+resource "aws_route_table" "wp_public_rt" {
+        vpc_id = "${aws_vpc.wp_vpc.id}"
+
+        route{
+          cidr_block = "0.0.0.0/0"
+          gateway_id = "${aws_internet_gateway.wp_internet_gateway.id}"      
+        }
+                tag {
+                Name = "wp_piblic"   
+               }
+               }
+
+resource "aws_default_route_table" "wp_private_rt"{
+   default_route_table_id = "${aws_vpc.wp_vpc.default_route_table_id}"
+   tags {
+   Name = "wp_public"
+
+
+   }
+
+
 }
 
