cc99cba5c22e0b1541259bf4e2f7fe1f4b2dfc80
vmpowercli <vmpowercli@gmail.com>
2018-04-29 14:32:28 -0700

    Remove . from RDS Subnet

--- main.tf
+++ main.tf
@@ -197,7 +197,7 @@ resource "aws_subnet" "wp_rds3_subnet" {
 
 resource "aws_db_subnet_group" "wp_rds_subnetgroup"{
         name ="wp_rds_subnetgroup"
-        subnet_ids = [ "${aws_subnet.wp_rds1._subnet.id}",
+        subnet_ids = [ "${aws_subnet.wp_rds1_subnet.id}",
         "${aws_subnet.wp_rds2_subnet.id}",
         "${aws_subnet.wp.rds3_subnet.id}"
         ]
