50b52c7e22bb2322e2c37c87d6cea3504ec56876
vmpowercli <vmpowercli@gmail.com>
2018-04-29 09:51:42 -0700

    Removed Network.tf

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ resource "aws_vpc" "wp_vpc"{
 
 #Internet GW
 
-recource "aws_internet_gateway" "wp_internet_gateway"{
+resource "aws_internet_gateway" "wp_internet_gateway"{
 	vpc_id = "${aws_vpc.wp_vpc.id}"
 	
 	tags {
