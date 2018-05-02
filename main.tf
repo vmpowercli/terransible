344328390d0256670af824528c8a5742f03e2db2
vmpowercli <vmpowercli@gmail.com>
2018-05-02 13:54:58 -0700

    Another terraform fmt --diff

--- main.tf
+++ main.tf
@@ -214,107 +214,95 @@ resource "aws_route_table_association" "wp_private2_assoc" {
 
 ##### Security Group #####
 
-resource "aws_security_group" "wp_dev_sg"{
-  name = "wp_dev_sg"
+resource "aws_security_group" "wp_dev_sg" {
+  name        = "wp_dev_sg"
   description = "Used for access to the dev instance"
-  vpc_id = "${aws_vpc.wp_vpc.id}"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
 
-#### SSH #####
-ingress {
-  from_port = 22
-  to_port   = 22
-  protocol = "tcp"
-  cidr_blocks = ["${var.localip}"]
+  #### SSH #####
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.localip}"]
   }
-#### HTTP #####
-ingress {
-from_port = 80
-to_port   = 80
-protocol  = "tcp"
-cidr_blocks = ["${var.localip}"]
-}  
 
-egress {
+  #### HTTP #####
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
 
-from_port = 0
-to_port =   0
-protocol = "-1"
-cidr_blocks = ["0.0.0.0/0"]
-}
-
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 #### Public Security Group #####
 
-resource "aws_security_group" "wp_public_sg"{
-name = "wp_public_sg"
-description = "Used for the ELP for Public access"
-vpc_id = "${aws_vpc.wp_vpc.id}"
+resource "aws_security_group" "wp_public_sg" {
+  name        = "wp_public_sg"
+  description = "Used for the ELP for Public access"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
 
-#### HTTP #####
-ingress {
+  #### HTTP #####
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-from_port = 80
-to_port   = 80
-protocol  = "tcp"
-cidr_blocks  = ["0.0.0.0/0"]
-}
-
-egress {
-
-from_port = 0
-to_port =   0
-protocol = "-1"
-cidr_blocks = ["0.0.0.0/0"]
-}
-
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 #### Private Security Group #####
 
 resource "aws_security_group" "wp_private_sg" {
-name = "wp_private_sg"
-description = "Used for Private Instances"
-vpc_id = "${aws_vpc.wp_vpc.id}"
+  name        = "wp_private_sg"
+  description = "Used for Private Instances"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
 
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    port        = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-ingress {
-
-from_port = 0
-to_port = 0
-port = "-1"
-cidr_blocks = ["0.0.0.0/0"]
-}
-
-egress {
-
-from_port = 0
-to_port = 0
-port = "-1"
-cidr_blocks = ["0.0.0.0/0"]
-}
-
+  egress {
+    from_port   = 0
+    to_port     = 0
+    port        = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 #### RDS Security Group ####
-resource "aws_security_group" "wp_rds_sg"{
-name = "wp_rds_sg"
-description = "Used for RDS Instances"
+resource "aws_security_group" "wp_rds_sg" {
+  name        = "wp_rds_sg"
+  description = "Used for RDS Instances"
 
-#### SQL Access from Public/Private Security Group ####
+  #### SQL Access from Public/Private Security Group ####
 
-ingress  {
-  from_port = 3306
-  to_port = 3306
-  protocol = "tcp"
+  ingress {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
 
-security_groups = ["${aws_security_group.wp_dev_sg.id}",
-                    "${aws_security_group.wp_public_sg.id}",
-                      "${aws_security_group.wp_private_sg.id}"]  
+    security_groups = ["${aws_security_group.wp_dev_sg.id}",
+      "${aws_security_group.wp_public_sg.id}",
+      "${aws_security_group.wp_private_sg.id}",
+    ]
   }
-
 }
-
-
-
-
