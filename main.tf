789a41d8dc56cc26cffec65074e6d95b952b8ab2
vmpowercli <vmpowercli@gmail.com>
2018-05-02 17:29:53 -0700

    terraform fmt --diff

--- main.tf
+++ main.tf
@@ -349,16 +349,15 @@ resource "aws_s3_bucket" "code" {
 }
 
 #### RDS #####
-resource "aws_db_instance" "wp_db"{
-
-allocated_storage = 10
-engine = "mysql"
-engine_version = "5.6.27"
-instance_class = "${var.db_instance_class}"
-name = "${var.dbname}"
-username = "{var.dbpassword}"
-password = "{var.dbpassword}"
-db_subnet_group_name= "${aws_db_subnet_group.wp_rds_subnetgroup.name}"
-vpc_security_group_ids= ["aws_security_group.wp_rds_sg.id"]
-skip_final_snapshot = true
+resource "aws_db_instance" "wp_db" {
+  allocated_storage      = 10
+  engine                 = "mysql"
+  engine_version         = "5.6.27"
+  instance_class         = "${var.db_instance_class}"
+  name                   = "${var.dbname}"
+  username               = "{var.dbpassword}"
+  password               = "{var.dbpassword}"
+  db_subnet_group_name   = "${aws_db_subnet_group.wp_rds_subnetgroup.name}"
+  vpc_security_group_ids = ["aws_security_group.wp_rds_sg.id"]
+  skip_final_snapshot    = true
 }
