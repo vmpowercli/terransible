28940dd2b15a4ee6954c632affacba62f79d562a
vmpowercli <vmpowercli@gmail.com>
2018-05-02 17:07:50 -0700

    terrform fmt --diff ++

--- main.tf
+++ main.tf
@@ -310,15 +310,15 @@ resource "aws_security_group" "wp_rds_sg" {
 
 #### VPC Endpoint for S3 ####
 
-resource "aws_vpc_endpoint" "wp_private_s3_endpoint"{
+resource "aws_vpc_endpoint" "wp_private_s3_endpoint" {
+  vpc_id       = "${aws_vpc.wp_vpc.id}"
+  service_name = "com.amazonaws.${var.aws_region}.s3"
 
-vpc_id = "${aws_vpc.wp_vpc.id}"
-service_name = "com.amazonaws.${var.aws_region}.s3"
-route_table_ids = ["${aws_vpc.wp_vpc.main_route_table_id}",
-                    "${aws_route_table.wp_public_rt.id}"
-                    ]
+  route_table_ids = ["${aws_vpc.wp_vpc.main_route_table_id}",
+    "${aws_route_table.wp_public_rt.id}",
+  ]
 
-policy = <<POLICY
+  policy = <<POLICY
 {
   "Statement": [
     {
@@ -334,17 +334,16 @@ POLICY
 
 ##### S3 Code Bucket ####
 
-resource "random_id" "wp_code_bucket"{
-byte_length = 2
+resource "random_id" "wp_code_bucket" {
+  byte_length = 2
 }
 
-resource "aws_s3_bucket" "code"{
-  bucket = "${var.domain_name}-${random_id.wp_code_bucket.dec}"
-  acl = "private"
+resource "aws_s3_bucket" "code" {
+  bucket        = "${var.domain_name}-${random_id.wp_code_bucket.dec}"
+  acl           = "private"
   force_destroy = true
+
   tags {
     name = "code bucket"
- }
+  }
 }
-
-
