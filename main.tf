aee4df0763c76027d4f003ecaf8ea203ce2839b2
vmpowercli <vmpowercli@gmail.com>
2018-05-03 21:10:03 -0400

    Typo Sg to ASG

--- main.tf
+++ main.tf
@@ -475,8 +475,8 @@ lifecycle {
 }
 
 #### Auto Scaling Group ####
-resource "aws_autoscaling_group" "wp_sg"{
-  name = "asg-${self.id}"
+resource "aws_autoscaling_group" "wp_asg"{
+  name = "asg-${aws_launch_configuration.wp_lc.id}"
   max_size = "${var.asg_max}"
   min_size = "${var.asg_min}"
   health_check_grace_period = "${var.asg_grace}"
@@ -488,7 +488,7 @@ resource "aws_autoscaling_group" "wp_sg"{
   vpc_zone_identifier = ["${aws_subnet.wp_private1_subnet.id}",
                           "${aws_subnet.wp_private2_subnet.id}",
                           ]
-launch_configuration = "${self.name}"
+launch_configuration = "${aws_launch_configuration.wp_lc.name}"
 
 tag {
   key = "Name"
